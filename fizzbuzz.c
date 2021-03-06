1de6cb3429907ffdfac9f71ec8958f6b0f8e711d
victorgreat4 <vcl@telia.com>
2021-03-06 10:35:17 +0100

    second commit added helper function

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+int fbindex(int n)
+{
+int i = 0;
+if(n % 3 == 0) i += 1;
+if(n % 5 == 0) i += 2;
+return i; 
+}
 
-int main(){
+int main(void){
 /*
 First solo upload test!
 */
