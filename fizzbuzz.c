18acc6cac0605a69959ffdea957c41a05d3e39c7
victorgreat4 <vcl@telia.com>
2021-03-09 09:25:01 +0100

    Added input for the user!

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -8,14 +8,18 @@ return i;
 }
 
 int main(void){
+	int n = 0;
+	printf("How long do you want fizzbuzz to be?\n");
+	scanf("%d",&n);
 /*
 First solo upload test!
 */
 const char *fb[] = {"%d\n", "Fizz\n", "Buzz\n", "FizzBuzz\n"};
-for(int i = 1; i <= 100; i++)
+for(int i = 1; i <= n; i++)
 {
 printf(fb[fbindex(i)],i);
 }
+
 
 return 0;
 }
